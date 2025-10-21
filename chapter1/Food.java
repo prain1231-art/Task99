fd53e2e9f3c4d3efdbd62555ef5a33a9da455c49
prain1231-art <prain1231@gmail.com>
2025-10-21 13:37:49 +0000

    1st Commit

---
+++ chapter1/Food.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+public class Food {
+public static void main(String[] args){
+        System.out.println("Hi I'm Food");
+    }
+}
---
+++ chapter1/Main.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Kamusta Mundo");
+    }
+}
+
+
